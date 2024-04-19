b7d39af66550ac37a0bf255a78e75fef7098aec5
Mykhailo Alekseiev <mykhailo.alekseiev@esto.eu>
2024-04-19 14:40:56 +0300

    add output for custom js action

--- .github/actions/deploy-s3-javascript/main.cjs
+++ .github/actions/deploy-s3-javascript/main.cjs
@@ -16,7 +16,9 @@ function run (){
 
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-  core.notice('Hello from my custom Javascript Action!')
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+
+  core.setOutput('website-url', websiteUrl)
 }
 
 run()
