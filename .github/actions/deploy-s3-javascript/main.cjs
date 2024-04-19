f65e3f141f0d03bb5579c77422a74d30a4ba0abf
Mykhailo Alekseiev <mykhailo.alekseiev@esto.eu>
2024-04-19 14:43:07 +0300

    fix action

--- .github/actions/deploy-s3-javascript/main.cjs
+++ .github/actions/deploy-s3-javascript/main.cjs
@@ -16,7 +16,7 @@ function run (){
 
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`
 
   core.setOutput('website-url', websiteUrl)
 }
